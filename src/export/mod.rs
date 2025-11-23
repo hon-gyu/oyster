b68e779e2736fe30e17357a4ffaec5458f60f6a4
hongyu <linerwang1997@gmail.com>
2025-11-23 14:07:53 +0000

    bring back cli

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -4,6 +4,6 @@ pub mod template;
 pub mod types;
 pub mod writer;
 
-// pub use generator::generate_site;
-// pub use types::*;
-// pub use writer::{push_html, write_html_fmt, write_html_io};
+pub use generator::generate_site;
+pub use types::*;
+pub use writer::{push_html, write_html_fmt, write_html_io};
--- src/export/types.rs
+++ src/export/types.rs
@@ -10,8 +10,6 @@ pub struct SiteConfig {
     pub base_url: String,
     /// Output directory for generated site
     pub output_dir: PathBuf,
-    // /// Whether to generate backlinks
-    // pub generate_backlinks: bool,
 }
 
 impl Default for SiteConfig {
@@ -20,7 +18,6 @@ impl Default for SiteConfig {
             title: "".to_string(),
             base_url: "/".to_string(),
             output_dir: PathBuf::from("dist"),
-            // generate_backlinks: true,
         }
     }
 }
@@ -56,53 +53,3 @@ pub struct SiteContext {
     pub title: String,
     pub base_url: String,
 }
-
-// #[derive(Debug, Clone, Serialize)]
-// struct LinkInfo {
-//     title: String,
-//     path: String,
-// }
-
-// <review>
-
-// /// Link information for a page
-// #[derive(Debug, Clone, Serialize)]
-// pub struct LinkContext {
-//     /// Pages that link to this page
-//     pub backlinks: Vec<BacklinkInfo>,
-//     /// Pages this page links to
-//     pub outgoing_links: Vec<OutgoingLinkInfo>,
-// }
-
-// /// Information about a backlink
-// #[derive(Debug, Clone, Serialize)]
-// pub struct BacklinkInfo {
-//     /// Title of the page that links here
-//     pub title: String,
-//     /// Path to the page
-//     pub path: String,
-// }
-
-// /// Information about an outgoing link
-// #[derive(Debug, Clone, Serialize)]
-// pub struct OutgoingLinkInfo {
-//     /// Title of the linked page
-//     pub title: String,
-//     /// Path to the page
-//     pub path: String,
-// }
-
-// // </review> Is this needed? Can't we just the one in link.rs?
-
-// /// Table of contents entry
-// #[derive(Debug, Clone, Serialize)]
-// pub struct TocEntry {
-//     /// Heading level (1-6)
-//     pub level: u8,
-//     /// Heading text
-//     pub text: String,
-//     /// Anchor ID for linking
-//     pub id: String,
-// }
-
-// // <review/> can't we just use the one in heading.rs?
