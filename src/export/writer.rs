d0d51aacf6bba669ecb28a306b4bb3f73acee0c7
hongyu <linerwang1997@gmail.com>
2025-11-28 01:07:33 +0000

    link katex style

--- src/export/writer.rs
+++ src/export/writer.rs
@@ -210,6 +210,7 @@ pub fn render_vault(
             head {
                 meta charset="utf-8";
                 meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css" integrity="sha384-wcIxkf4k558AjM3Yz3BBFQUbk/zgIYC2R0QpeeYb+TwlBVMrlgLqwRjRtGZiK7ww" crossorigin="anonymous";
                 title { "Home" }
                 style {
                     (PreEscaped(get_style(theme)))
@@ -245,6 +246,7 @@ fn render_page(
             head {
                 meta charset="utf-8";
                 meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css" integrity="sha384-wcIxkf4k558AjM3Yz3BBFQUbk/zgIYC2R0QpeeYb+TwlBVMrlgLqwRjRtGZiK7ww" crossorigin="anonymous";
                 title { (title) }
                 style {
                     (PreEscaped(style))
