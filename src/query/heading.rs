02ca318424288b86d8cb8eb1a746a606360dbab1
hongyu <linerwang1997@gmail.com>
2026-01-12 23:15:44 +0000

    impl hierarchical for heading

--- src/query/heading.rs
+++ src/query/heading.rs
@@ -1,6 +1,6 @@
+use crate::hierarchy::Hierarchical;
 use pulldown_cmark::HeadingLevel;
 use serde::{Deserialize, Serialize};
-use tree_sitter::Point;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Heading {
@@ -8,9 +8,17 @@ pub struct Heading {
     pub text: String,
     pub start_byte: usize,
     pub end_byte: usize,
+    /// column and row
     pub start_point: (usize, usize),
+    /// column and row
     pub end_point: (usize, usize),
     pub id: Option<String>,
     pub classes: Vec<String>,
     pub attrs: Vec<(String, Option<String>)>,
+}
+
+impl Hierarchical for Heading {
+    fn level(&self) -> usize {
+        self.level as usize
+    }
 }
