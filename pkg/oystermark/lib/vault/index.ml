4f6c1a98c5bad6aa9dd8364a7007925401bf7dc3
hongyu <linerwang1997@gmail.com>
2026-03-05 21:20:10 +0000

    vault wrapped module

--- pkg/oystermark/lib/vault/index.ml
+++ pkg/oystermark/lib/vault/index.ml
@@ -82,35 +82,6 @@ let rec list_files_recursive ~(root : string) ~(rel_prefix : string) : string li
       | _ -> [ rel_path ]))
 ;;
 
-(** Build a vault index from a root directory.
-    Returns the index and a list of [(rel_path, doc)] pairs for each markdown file,
-    where each [doc] has already been through pass-1 parsing (wikilinks + block IDs). *)
-let build (vault_root : string) : t * (string * Cmarkit.Doc.t) list =
-  let all_files = list_files_recursive ~root:vault_root ~rel_prefix:"" in
-  let files_and_docs =
-    List.filter_map all_files ~f:(fun rel_path ->
-      if String.is_suffix rel_path ~suffix:".md"
-      then (
-        let full_path = Filename.concat vault_root rel_path in
-        let content = In_channel.read_all full_path in
-        let doc = Oystermark_base.of_string content in
-        let headings = extract_headings doc in
-        let block_ids = extract_block_ids doc in
-        Some ({ rel_path; headings; block_ids }, (rel_path, doc)))
-      else None)
-  in
-  let files =
-    let non_md =
-      List.filter_map all_files ~f:(fun rel_path ->
-        if String.is_suffix rel_path ~suffix:".md"
-        then None
-        else Some { rel_path; headings = []; block_ids = [] })
-    in
-    List.map files_and_docs ~f:fst @ non_md
-  in
-  { files }, List.map files_and_docs ~f:snd
-;;
-
 let%expect_test "extract_headings" =
   let md =
     {|
