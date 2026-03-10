baf1d46d34ab063cdef7e9b9f109a320cd253abc
hongyu <linerwang1997@gmail.com>
2026-03-10 20:34:22 +0000

    cleanup cmarkit and omark grammar

--- pkg/cmarkit/src/tool/cmd_latex.mli
+++
@@ -1,7 +0,0 @@
-(*---------------------------------------------------------------------------
-   Copyright (c) 2023 The cmarkit programmers. All rights reserved.
-   SPDX-License-Identifier: ISC
-  ---------------------------------------------------------------------------*)
-
-val cmd : Cmdliner.Cmd.Exit.code Cmdliner.Cmd.t
-(** [cmd] is the command line for [latex]. *)
