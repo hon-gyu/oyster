b35199a9ca85fa61421fa38890ff13ab8d755f1d
hongyu <linerwang1997@gmail.com>
2026-03-14 17:40:01 +0000

    DOCS: odocs

---
+++ pkg/oystermark/doc/note/md_to_mld.ml
@@ -0,0 +1,20 @@
+open Core
+
+let () =
+  let md = Sys.get_argv () |> Array.last in
+  let title =
+    String.chop_suffix_exn md ~suffix:".md"
+    |> List.return
+    |> List.concat_map ~f:(fun s -> String.split s ~on:'_')
+    |> List.concat_map ~f:(fun s -> String.split s ~on:'-')
+    |> List.map ~f:(fun s -> String.capitalize s)
+    |> String.concat ~sep:" "
+  in
+  let open Shexp_process in
+  let open Shexp_process.Infix in
+  eval
+    (printf "{0 %s}\n" title
+     >> printf "{v\n"
+     >> run "cat" [md]
+     >> printf "v}\n")
+;;
---
+++ pkg/oystermark/doc/note_toc.ml
@@ -0,0 +1,23 @@
+(* Walk note/ dir, find all .md files, and prints a list *)
+
+open Core
+module List' = List
+module String' = String
+
+let () =
+  let open Shexp_process in
+  let open Shexp_process.Infix in
+  eval
+    (run "find" [ "note"; "-name"; "*.md"; "-type"; "f" ]
+     |> capture_unit [ Stdout ]
+     >>= fun output ->
+     let files =
+       String'.split output ~on:'\n'
+       |> List'.filter ~f:(fun s -> not (String'.is_empty s))
+       |> List'.sort ~compare:String'.compare
+     in
+     List'.fold_left files ~init:(return ()) ~f:(fun acc file ->
+       acc
+       >> printf "- {!page-note/%s}\n" (Filename.chop_extension (Filename.basename file)))
+    )
+;;
