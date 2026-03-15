875118f82c1c720a50b161a6b861b42b4da3d648
hongyu <linerwang1997@gmail.com>
2026-03-15 00:10:31 +0000

    pandoc attribute mld

--- pkg/oystermark/doc/note_toc.ml
+++ pkg/oystermark/doc/note_toc.ml
@@ -8,16 +8,19 @@ let () =
   let open Shexp_process in
   let open Shexp_process.Infix in
   eval
-    (run "find" [ "note"; "-name"; "*.md"; "-type"; "f" ]
-     |> capture_unit [ Stdout ]
-     >>= fun output ->
-     let files =
-       String'.split output ~on:'\n'
-       |> List'.filter ~f:(fun s -> not (String'.is_empty s))
-       |> List'.sort ~compare:String'.compare
-     in
-     List'.fold_left files ~init:(return ()) ~f:(fun acc file ->
-       acc
-       >> printf "- {!page-\"%s\"}\n" (Filename.chop_extension (Filename.basename file)))
-    )
+    (print "{ul\n"
+     >> (run "find" [ "note"; "-name"; "*.md"; "-type"; "f" ]
+         |> capture_unit [ Stdout ]
+         >>= fun output ->
+         let files =
+           String'.split output ~on:'\n'
+           |> List'.filter ~f:(fun s -> not (String'.is_empty s))
+           |> List'.sort ~compare:String'.compare
+         in
+         List'.fold_left files ~init:(return ()) ~f:(fun acc file ->
+           acc
+           >> printf
+                "  {- {!page-\"%s\"}}\n"
+                (Filename.chop_extension (Filename.basename file))))
+     >> print "}\n")
 ;;
