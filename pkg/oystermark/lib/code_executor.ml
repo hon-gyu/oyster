6e3e8e2b0d145abd9d76e12dedc64558264b8f3b
hongyu <linerwang1997@gmail.com>
2026-03-17 22:02:47 +0000

    uv executor

--- pkg/oystermark/lib/code_executor.ml
+++ pkg/oystermark/lib/code_executor.ml
@@ -9,13 +9,13 @@ type cell =
   ; info : Attribute.t option
   ; content : string
   }
-  [@@deriving sexp_of]
+[@@deriving sexp_of]
 
 type output =
   { id : int
   ; res : [ `Html of string | `Markdown of string | `Error of string ]
   }
-  [@@deriving sexp_of]
+[@@deriving sexp_of]
 
 type exec_ctx =
   { config : Yaml.value
@@ -39,12 +39,13 @@ let extract_code_blocks (doc : Cmarkit.Doc.t) : cell list =
         |> String.concat ~sep:"\n"
       in
       let cb_info = Cmarkit.Meta.find Attribute.meta_key meta in
-      let cell = {
-        id = !block_id;
-        lang = (cb_info |> Option.map ~f:(fun ci -> ci.lang));
-        info = (cb_info |> Option.map ~f:(fun ci -> ci.attribute));
-        content;
-      } in
+      let cell =
+        { id = !block_id
+        ; lang = cb_info |> Option.map ~f:(fun ci -> ci.lang)
+        ; info = cb_info |> Option.map ~f:(fun ci -> ci.attribute)
+        ; content
+        }
+      in
       incr block_id;
       Cmarkit.Folder.ret (cell :: acc)
     | _ -> Cmarkit.Folder.default
@@ -54,7 +55,9 @@ let extract_code_blocks (doc : Cmarkit.Doc.t) : cell list =
 ;;
 
 let%expect_test "extract_code_blocks" =
-  let doc = Parse.of_string {|
+  let doc =
+    Parse.of_string
+      {|
 Hi
 ```python
 print("Hello")
@@ -63,14 +66,16 @@ print("Hello")
 ```bash {.foo baz=zzz}
 bar
 ```
-|} in
+|}
+  in
   let cells = extract_code_blocks doc in
   print_s [%sexp (cells : cell list)];
-  [%expect {|
+  [%expect
+    {|
     (((id 0) (lang ()) (info ()) (content "print(\"Hello\")"))
      ((id 1) (lang (bash)) (info (((id ()) (classes (.foo)) (kvs ((baz zzz))))))
       (content bar)))
-    |}];
+    |}]
 ;;
 
 let extract_exec_ctx (doc : Cmarkit.Doc.t) : exec_ctx =
@@ -85,7 +90,7 @@ let extract_exec_ctx (doc : Cmarkit.Doc.t) : exec_ctx =
     | None -> Yaml.(`O [])
   in
   let inputs = extract_code_blocks doc in
-  {config; inputs}
+  { config; inputs }
 ;;
 
 (* uv
@@ -97,37 +102,73 @@ type uv_config =
   }
 
 let default_uv_config = { version = 3.13; dependencies = [] }
-let uv (default_config : uv_config) : executor = todo ()
+
+let uv_config_of_config (config : Yaml.value) : uv_config =
+  match Yaml.Util.find "pyproject" config with
+  | Ok (Some (`O fields)) ->
+    let version =
+      match List.Assoc.find fields ~equal:String.equal "version" with
+      | Some (`Float v) -> v
+      | Some (`String s) -> Float.of_string s
+      | _ -> default_uv_config.version
+    in
+    let dependencies =
+      match List.Assoc.find fields ~equal:String.equal "dependencies" with
+      | Some (`A deps) ->
+        List.filter_map deps ~f:(function
+          | `String s -> Some s
+          | _ -> None)
+      | _ -> default_uv_config.dependencies
+    in
+    { version; dependencies }
+  | _ -> default_uv_config
+;;
 
 let make_notebook cells =
   let make_cell source =
-    `Assoc [
-      "cell_type", `String "code";
-      "source", `List (List.map source ~f:(fun s -> `String s));
-      "metadata", `Assoc [];
-      "outputs", `List [];
-      "execution_count", `Null;
-    ]
-  in
-  `Assoc [
-    "nbformat", `Int 4;
-    "nbformat_minor", `Int 5;
-    "metadata", `Assoc [
-      "kernelspec", `Assoc [
-        "display_name", `String "Python 3";
-        "language", `String "python";
-        "name", `String "python3";
+    `Assoc
+      [ "cell_type", `String "code"
+      ; "source", `List (List.map source ~f:(fun s -> `String s))
+      ; "metadata", `Assoc []
+      ; "outputs", `List []
+      ; "execution_count", `Null
       ]
-    ];
-    "cells", `List (List.map cells ~f:make_cell);
-  ]
+  in
+  `Assoc
+    [ "nbformat", `Int 4
+    ; "nbformat_minor", `Int 5
+    ; ( "metadata"
+      , `Assoc
+          [ ( "kernelspec"
+            , `Assoc
+                [ "display_name", `String "Python 3"
+                ; "language", `String "python"
+                ; "name", `String "python3"
+                ] )
+          ] )
+    ; "cells", `List (List.map cells ~f:make_cell)
+    ]
+;;
 
-let run_notebook ~venv ~nb_json : (Yojson.Basic.t, string) result =
+let run_notebook ~(uv_config : uv_config) ~(nb_json : Yojson.Basic.t)
+  : (Yojson.Basic.t, string) result
+  =
   let tmp_in = Filename_unix.temp_file "nb_in" ".ipynb" in
   let tmp_out = Filename_unix.temp_file "nb_out" ".ipynb" in
   Yojson.Basic.to_file tmp_in nb_json;
-  let cmd = sprintf "%s/bin/jupyter nbconvert --to notebook --execute %s --output %s 2>/dev/null"
-    venv tmp_in tmp_out
+  let with_args =
+    "jupyter" :: uv_config.dependencies
+    |> List.map ~f:(fun dep -> sprintf "--with %s" dep)
+    |> String.concat ~sep:" "
+  in
+  let cmd =
+    sprintf
+      "uv run --python %g %s jupyter nbconvert --to notebook --execute %s --output %s \
+       2>/dev/null"
+      uv_config.version
+      with_args
+      tmp_in
+      tmp_out
   in
   match Core_unix.system cmd with
   | Ok () ->
@@ -135,15 +176,17 @@ let run_notebook ~venv ~nb_json : (Yojson.Basic.t, string) result =
     Sys_unix.remove tmp_in;
     Sys_unix.remove tmp_out;
     Ok result
-  | Error _ ->
-    Error "nbconvert failed"
+  | Error _ -> Error "nbconvert failed"
+;;
 
 let cell_outputs (cell : Yojson.Basic.t) : string list =
   let open Yojson.Basic.Util in
-  cell |> member "outputs" |> to_list |> List.filter_map ~f:(fun output ->
+  cell
+  |> member "outputs"
+  |> to_list
+  |> List.filter_map ~f:(fun output ->
     match output |> member "output_type" |> to_string with
-    | "stream" ->
-      Some (output |> member "text" |> to_string)
+    | "stream" -> Some (output |> member "text" |> to_string)
     | "execute_result" | "display_data" ->
       Some (output |> member "data" |> member "text/plain" |> to_string)
     | "error" ->
@@ -151,6 +194,7 @@ let cell_outputs (cell : Yojson.Basic.t) : string list =
       let evalue = output |> member "evalue" |> to_string in
       Some (sprintf "%s: %s" ename evalue)
     | _ -> None)
+;;
 
 let notebook_outputs (nb_json : Yojson.Basic.t) : string list list =
   let open Yojson.Basic.Util in
@@ -159,3 +203,28 @@ let notebook_outputs (nb_json : Yojson.Basic.t) : string list list =
   |> to_list
   |> List.filter ~f:(fun c -> String.equal (c |> member "cell_type" |> to_string) "code")
   |> List.map ~f:cell_outputs
+;;
+
+let uv_executor ?(attr_filter : Attribute.t option -> bool = fun _ -> true) : executor =
+  fun ctx ->
+  let uv_config = uv_config_of_config ctx.config in
+  let (python_cells : cell list) =
+    List.filter ctx.inputs ~f:(fun cell ->
+      match cell.lang with
+      | Some l ->
+        let l' = l |> String.lowercase in
+        let is_py = String.equal l' "python" || String.equal l' "py" in
+        is_py && attr_filter cell.info
+      | None -> false)
+  in
+  let (sources : string list list) =
+    List.map python_cells ~f:(fun cell -> String.split_lines cell.content)
+  in
+  let nb_json = make_notebook sources in
+  match run_notebook ~uv_config ~nb_json with
+  | Error msg -> List.map python_cells ~f:(fun cell -> { id = cell.id; res = `Error msg })
+  | Ok executed ->
+    let outputs = notebook_outputs executed in
+    List.map2_exn python_cells outputs ~f:(fun cell outs ->
+      { id = cell.id; res = `Markdown (String.concat ~sep:"\n" outs) })
+;;
