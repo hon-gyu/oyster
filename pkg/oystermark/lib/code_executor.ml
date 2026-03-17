822bd6c8ebd9b0f13eb13a46601793d61df9ff57
hongyu <linerwang1997@gmail.com>
2026-03-17 22:13:48 +0000

    TEST: uv executor

--- pkg/oystermark/lib/code_executor.ml
+++ pkg/oystermark/lib/code_executor.ml
@@ -100,6 +100,7 @@ type uv_config =
   { version : float
   ; dependencies : string list
   }
+[@@deriving sexp_of]
 
 let default_uv_config = { version = 3.13; dependencies = [] }
 
@@ -124,11 +125,11 @@ let uv_config_of_config (config : Yaml.value) : uv_config =
   | _ -> default_uv_config
 ;;
 
-let make_notebook cells =
+let make_notebook (cells : string list) =
   let make_cell source =
     `Assoc
       [ "cell_type", `String "code"
-      ; "source", `List (List.map source ~f:(fun s -> `String s))
+      ; "source", `String source
       ; "metadata", `Assoc []
       ; "outputs", `List []
       ; "execution_count", `Null
@@ -154,7 +155,9 @@ let run_notebook ~(uv_config : uv_config) ~(nb_json : Yojson.Basic.t)
   : (Yojson.Basic.t, string) result
   =
   let tmp_in = Filename_unix.temp_file "nb_in" ".ipynb" in
-  let tmp_out = Filename_unix.temp_file "nb_out" ".ipynb" in
+  (* jupyter appends .ipynb to --output, so omit the extension here *)
+  let tmp_out_base = Filename_unix.temp_file "nb_out" "" in
+  let tmp_out = tmp_out_base ^ ".ipynb" in
   Yojson.Basic.to_file tmp_in nb_json;
   let with_args =
     "jupyter" :: uv_config.dependencies
@@ -163,20 +166,30 @@ let run_notebook ~(uv_config : uv_config) ~(nb_json : Yojson.Basic.t)
   in
   let cmd =
     sprintf
-      "uv run --python %g %s jupyter nbconvert --to notebook --execute %s --output %s \
-       2>/dev/null"
+      "JUPYTER_CONFIG_DIR=/dev/null uv run --python %g %s jupyter nbconvert --to notebook \
+       --execute %s --output %s 2>/dev/null"
       uv_config.version
       with_args
       tmp_in
-      tmp_out
+      tmp_out_base
   in
   match Core_unix.system cmd with
   | Ok () ->
     let result = Yojson.Basic.from_file tmp_out in
     Sys_unix.remove tmp_in;
+    Sys_unix.remove tmp_out_base;
     Sys_unix.remove tmp_out;
     Ok result
   | Error _ -> Error "nbconvert failed"
+;;
+
+(** Jupyter multiline_string: either a plain string or an array of strings *)
+let multiline_string (j : Yojson.Basic.t) : string =
+  let open Yojson.Basic.Util in
+  match j with
+  | `String s -> s
+  | `List _ -> j |> to_list |> List.map ~f:to_string |> String.concat ~sep:""
+  | _ -> ""
 ;;
 
 let cell_outputs (cell : Yojson.Basic.t) : string list =
@@ -186,9 +199,9 @@ let cell_outputs (cell : Yojson.Basic.t) : string list =
   |> to_list
   |> List.filter_map ~f:(fun output ->
     match output |> member "output_type" |> to_string with
-    | "stream" -> Some (output |> member "text" |> to_string)
+    | "stream" -> Some (output |> member "text" |> multiline_string)
     | "execute_result" | "display_data" ->
-      Some (output |> member "data" |> member "text/plain" |> to_string)
+      Some (output |> member "data" |> member "text/plain" |> multiline_string)
     | "error" ->
       let ename = output |> member "ename" |> to_string in
       let evalue = output |> member "evalue" |> to_string in
@@ -217,9 +230,7 @@ let uv_executor ?(attr_filter : Attribute.t option -> bool = fun _ -> true) : ex
         is_py && attr_filter cell.info
       | None -> false)
   in
-  let (sources : string list list) =
-    List.map python_cells ~f:(fun cell -> String.split_lines cell.content)
-  in
+  let sources = List.map python_cells ~f:(fun cell -> cell.content) in
   let nb_json = make_notebook sources in
   match run_notebook ~uv_config ~nb_json with
   | Error msg -> List.map python_cells ~f:(fun cell -> { id = cell.id; res = `Error msg })
@@ -227,4 +238,93 @@ let uv_executor ?(attr_filter : Attribute.t option -> bool = fun _ -> true) : ex
     let outputs = notebook_outputs executed in
     List.map2_exn python_cells outputs ~f:(fun cell outs ->
       { id = cell.id; res = `Markdown (String.concat ~sep:"\n" outs) })
+;;
+
+(* Unit test: dependency config is parsed from frontmatter *)
+let%expect_test "uv_config_of_config: parses version and dependencies" =
+  let config =
+    match
+      Yaml.of_string
+        {|
+pyproject:
+  version: "3.11"
+  dependencies:
+    - numpy
+    - pandas
+|}
+    with
+    | Ok v -> v
+    | Error (`Msg m) -> failwith m
+  in
+  let cfg = uv_config_of_config config in
+  print_s [%sexp (cfg : uv_config)];
+  [%expect {|
+    ((version 3.11) (dependencies (numpy pandas)))
+  |}]
+;;
+
+(* Integration tests: require uv + jupyter in PATH *)
+
+let%expect_test "uv_executor: basic" =
+  let ctx = extract_exec_ctx (Parse.of_string {|
+```python {}
+print("hello")
+```
+|}) in
+  print_s [%sexp (uv_executor ctx : output list)];
+  [%expect {|
+    (((id 0) (res (Markdown "hello\n"))))
+  |}]
+;;
+
+let%expect_test "uv_executor: python cells with other language in between" =
+  (* bash cell (id=1) is skipped; python cells (ids 0,2) share notebook state *)
+  let ctx =
+    extract_exec_ctx
+      (Parse.of_string
+         {|
+```python {}
+x = 1
+print(x)
+```
+```bash {}
+echo hi
+```
+```python {}
+print(x + 1)
+```
+|})
+  in
+  print_s [%sexp (uv_executor ctx : output list)];
+  [%expect {|
+    (((id 0) (res (Markdown "1\n"))) ((id 2) (res (Markdown "2\n"))))
+  |}]
+;;
+
+let%expect_test "uv_executor: attr_filter excludes matching cells" =
+  let ctx =
+    extract_exec_ctx
+      (Parse.of_string
+         {|
+```python {}
+print("runs")
+```
+```python {.no-exec}
+print("skipped")
+```
+|})
+  in
+  let outputs =
+    uv_executor
+      ~attr_filter:(fun attr ->
+        match attr with
+        | Some { classes; _ } ->
+          not (List.mem classes ".no-exec" ~equal:String.equal)
+        | None -> true)
+      ctx
+  in
+  print_s [%sexp (outputs : output list)];
+  [%expect {|
+    (((id 0) (res (Markdown "runs\n"))))
+  |}]
 ;;
